c0e0bd3b855c17d4a192c8af21f61e0ff3789e77
beryl76 <65161793+beryl76@users.noreply.github.com>
2023-09-07 06:13:16 +0700

    switch animation

---
+++ Assets/Script/New Script/Arrow/PlayerAnimationArrowController.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class PlayerAnimationArrowController : MonoBehaviour
+{
+    public Animator playerAnimator; // Reference to the player's Animator component.
+    public AnimatorOverrideController redArrowOverride;
+    public AnimatorOverrideController greenArrowOverride;
+    public AnimatorOverrideController blueArrowOverride;
+    public AnimatorOverrideController yellowArrowOverride;
+
+    private AnimatorOverrideController currentOverrideController;
+
+    private void Start()
+    {
+        // Ensure that playerAnimator is set in the Inspector.
+        if (playerAnimator == null)
+        {
+            Debug.LogError("Player Animation Controller: Player Animator reference not set.");
+            enabled = false; // Disable the script to avoid errors.
+            return;
+        }
+
+        // Set the initial override controller to Red Arrow.
+        currentOverrideController = redArrowOverride;
+        playerAnimator.runtimeAnimatorController = currentOverrideController;
+    }
+
+    public void ChangeArrowAnimation(string arrowType)
+    {
+        // Determine the appropriate Animator Override Controller based on the arrow type.
+        switch (arrowType)
+        {
+            case "RedArrow":
+                currentOverrideController = redArrowOverride;
+                break;
+            case "GreenArrow":
+                currentOverrideController = greenArrowOverride;
+                break;
+            case "BlueArrow":
+                currentOverrideController = blueArrowOverride;
+                break;
+            case "YellowArrow":
+                currentOverrideController = yellowArrowOverride;
+                break;
+            default:
+                Debug.LogError("Invalid arrow type: " + arrowType);
+                return;
+        }
+
+        // Apply the selected Animator Override Controller.
+        playerAnimator.runtimeAnimatorController = currentOverrideController;
+    }
+}
